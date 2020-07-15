c962600342be038f384deb8b39159f9792e2d192
Dmitriy Dzyuba <tzar4eg@gmail.com>
2020-07-15 14:57:28 +0300

    Вынес список товаров в отдельный модуль и добавил ленивую загрузку

--- src/app/goods/goods.module.ts
+++ src/app/goods/goods.module.ts
@@ -4,11 +4,12 @@ import { GoodsComponent } from './goods.component';
 import { GoodsListComponent } from './goods-list/goods-list.component';
 import { GoodsRoutingModule } from './goods-routing.module';
 import { MatTableModule } from '@angular/material/table';
+import { GoodsItemComponent } from './goods-item/goods-item.component';
 
 
 
 @NgModule({
-  declarations: [GoodsComponent, GoodsListComponent],
+  declarations: [GoodsComponent, GoodsListComponent, GoodsItemComponent],
   imports: [
     CommonModule,
     MatTableModule,
