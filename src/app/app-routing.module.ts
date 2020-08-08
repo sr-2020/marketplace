48c3a47222dbf4448935b0d0fc895b154251492b
dzu <tzar4eg@gmail.com>
2020-08-08 22:25:25 +0300

    fix debug component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,20 +6,19 @@ import { environment } from '../environments/environment'
 
 const routes: Routes = [
   { path: '', redirectTo: 'goods', pathMatch: 'full' },
-  { path: 'api/v1', component: NotFoundComponent },
   { path: 'goods', loadChildren: () => import('./goods/goods.module').then(m => m.GoodsModule), },
   {
     path: 'transactions',
     loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsModule),
   },
-  { path: '404', component: NotFoundComponent },
-  { path: '**', redirectTo: '404' }
 ]
 
 if (!environment.production) {
   routes.push({ path: 'debug', loadChildren: () => import('./debug/debug.module').then(m => m.DebugModule), },
   )
 }
+routes.push({ path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' })
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/debug/debug.component.ts
+++ src/app/debug/debug.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 
 @Component({
@@ -7,13 +7,18 @@ import { HttpClient } from '@angular/common/http'
   styleUrls: ['./debug.component.scss']
 })
 export class DebugComponent implements OnInit {
+  req: any
 
-  constructor(private httpService: HttpClient) { }
-  sendTestReq() {
-    this.httpService.get('https://gateway.evarun.ru/api/v1/billing/api/test/testid', { withCredentials: true }).subscribe(el => {
-      console.log(el);
-    });
+  constructor(private httpService: HttpClient) {
   }
+
+  sendTestReq() {
+    this.httpService.get('https://gateway.evarun.ru/api/v1/billing/api/test/testid', { withCredentials: true })
+      .subscribe(
+        el => console.log(el),
+        error => console.log(error))
+  }
+
   ngOnInit(): void {
   }
 
