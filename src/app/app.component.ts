13bbdde903400698c1d9a9647a3077539d40b392
dzu <tzar4eg@gmail.com>
2020-08-09 13:54:35 +0300

    added models, types and session service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core'
 import { AppService } from './app.service';
+import { SessionService } from './services/session.service'
 
 @Component({
   selector: 'sr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private _appService: AppService) {}
+export class AppComponent implements OnInit{
+  constructor(private _appService: AppService, private _sessionService: SessionService) {}
+  ngOnInit(): void {
+    this._sessionService.initSession()
+  }
 
   @HostListener('window:resize')
   onResize() {
     this._appService.isMobile = window.innerWidth < 767;
   }
+
 
 }
---
+++ src/app/types/lifestyle.type.ts
@@ -0,0 +1,3 @@
+export type LifestyleType = 'Wood' | 'Bronze' | 'Silver'| 'Gold' | 'Platinum' | 'Irridium'
+
+//'дерево', 'бронза', 'серебро', 'золото', 'платина', 'ирридиум'
