99f23365799cab137ab6946190fcc52dd6a5dafd
dzu <tzar4eg@gmail.com>
2020-08-09 14:22:55 +0300

    minor refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,28 @@
 import { Component, HostListener, OnInit } from '@angular/core'
-import { AppService } from './app.service';
+import { AppService } from './app.service'
 import { SessionService } from './services/session.service'
 import { SessionModel } from './models/session.model'
 
 @Component({
   selector: 'sr-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(private _appService: AppService, private _sessionService: SessionService) {}
+export class AppComponent implements OnInit {
+  constructor(private _appService: AppService, private _sessionService: SessionService) {
+  }
+
   ngOnInit(): void {
     this._sessionService.initSession()
   }
 
   @HostListener('window:resize')
   onResize() {
-    this._appService.isMobile = window.innerWidth < 767;
+    this._appService.isMobile = window.innerWidth < 767
   }
 
   get session(): SessionModel {
-   return  this._sessionService.session
+    return this._sessionService.session
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { GoodsModule } from './goods/goods.module';
-import { SrCommonModule } from './common/common.module';
-import { MatButtonModule } from '@angular/material/button';
-import { TransactionsModule } from './transactions/transactions.module';
-import localeRu from '@angular/common/locales/ru';
-import { registerLocaleData } from '@angular/common';
-import { AppService } from './app.service';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NotFoundComponent } from './not-found/not-found.component'
+import { GoodsModule } from './goods/goods.module'
+import { SrCommonModule } from './common/common.module'
+import { MatButtonModule } from '@angular/material/button'
+import { TransactionsModule } from './transactions/transactions.module'
+import { registerLocaleData } from '@angular/common'
+import { AppService } from './app.service'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import localeRu from '@angular/common/locales/ru'
 
 registerLocaleData(localeRu, 'ru_RU');
 
@@ -23,8 +22,8 @@ registerLocaleData(localeRu, 'ru_RU');
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     SrCommonModule,
     GoodsModule,
     TransactionsModule,
