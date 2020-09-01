254785f3845ee1e2e94b7031ed600428bda78e03
Dmitriy Dzyuba <tzar4eg@gmail.com>
2020-09-01 18:10:12 +0300

    implemented offer model

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { OfferService } from './offer.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SessionService } from '../services/session.service';
+import { Component, OnInit } from '@angular/core'
+import { OfferService } from './offer.service'
+import { ActivatedRoute } from '@angular/router'
+import { SessionService } from '../services/session.service'
+import { OfferModel } from '../models/offer.model'
 
 @Component({
   selector: 'sr-offer',
@@ -9,29 +10,28 @@ import { SessionService } from '../services/session.service';
   styleUrls: ['./offer.component.scss']
 })
 export class OfferComponent implements OnInit {
-  offerId: string;
-  offer: any;
+  offerId: string
+  offer: OfferModel
 
-  isLoading = true;
 
   constructor(private _offerService: OfferService, private _route: ActivatedRoute, private _session: SessionService) {
   }
 
   ngOnInit(): void {
-    this.offerId = this._route.snapshot.params?.id;
+    this.offerId = this._route.snapshot.params?.id
     this.session$.subscribe(session => {
       if (!session) {
-        return;
+        return
       }
-      this.getOffer(this.offerId, session.currentCharacterId.toString());
-    });
+      this.getOffer(this.offerId, session.currentCharacterId.toString())
+    })
   }
 
   getOffer(id: string, charId: string) {
-    this._offerService.getRenta(id, charId).subscribe(offer => this.offer = offer);
+    this._offerService.getRenta(id, charId).subscribe(({ data }) => this.offer = data)
   }
 
   get session$() {
-    return this._session.session$;
+    return this._session.session$
   }
 }
--- src/app/offer/offer.service.ts
+++ src/app/offer/offer.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { SessionService } from '../services/session.service';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environments/environment'
+import { SessionService } from '../services/session.service'
+import { Observable } from 'rxjs'
+import { ResponseModel } from '../models/response.model'
+import { OfferModel } from '../models/offer.model'
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +14,10 @@ export class OfferService {
   constructor(private _httpClient: HttpClient, private _session: SessionService) {
   }
 
-  getRenta(id: string, charId: string) {
-    return this._httpClient.post<any>(`${ environment.api }shop/createpricebyqr`, {
+  getRenta(id: string, charId: string): Observable<ResponseModel<OfferModel>> {
+    return this._httpClient.post<ResponseModel<OfferModel>>(`${ environment.api }shop/createpricebyqr`, {
       character: charId,
       qr: id
-    });
+    })
   }
 }
