e35e32f34330d8b25a1d337fcf386b8bfc4def29
Dmitriy Dzyuba <tzar4eg@gmail.com>
2020-09-06 10:22:31 +0300

    implemented middleware webserver to proxy localhost request on evarun gateway

--- server/main.js
+++ server/main.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 8080
-// const {createProxyMiddleware} = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-// app.use('/proxy', createProxyMiddleware({target: 'https://gateway.evarun.ru/api/v1/billing/api/test/testid', changeOrigin: true}));
-app.use(express.static('dist'))
-
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html');
-});
+app.use('/', createProxyMiddleware({
+  target: 'https://gateway.evarun.ru/api/v1/billing/',
+  changeOrigin: true,
+  headers: {
+    'Cookie': 'Authorization=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0MTcxNCIsImF1dGgiOiJST0xFX1BMQVlFUiIsIm1vZGVsSWQiOjk1NzAsImV4cCI6MTU5OTM4NjkzNn0.MGbg_Vw5DhXTkjcoqKw6sE-kLC4aZvRlBikICTC-cBJdGcY6nh5gGT0gnUmYzm2VqDAzhv0UZb_wknUqO63rCw'
+  }
+}));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
